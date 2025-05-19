93de838ecd0ff9b8863ad4aeda2302f0dcf3f496
juliano-araujo <julianoaraujo@protonmail.com>
2025-05-19 12:22:53 -0300

    prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Header } from "@/components/header";
 import type { Metadata } from "next";
 import { Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/header";
 
 const poppinsSans = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
