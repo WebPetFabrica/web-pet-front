702f91b3f6a4c1593517706368f6bfb3847c85f4
juliano-araujo <julianoaraujo@protonmail.com>
2025-06-30 18:40:11 -0300

    feat: update fetch configuration to use NEXT_PUBLIC_API_URL and add session provider

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -3,14 +3,18 @@ import { createFetch } from "@better-fetch/fetch";
 import { getSession } from "next-auth/react";
 import { apiSchema } from "./api.schema";
 
+console.log("foda", process.env.NEXT_PUBLIC_API_URL);
+
 export const $fetch = createFetch({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   throw: true,
   schema: apiSchema,
   auth: {
     type: "Bearer",
     token: async () => {
       const session = await getSession();
+
+      console.log(session);
 
       const user = session?.user as User | undefined;
 
