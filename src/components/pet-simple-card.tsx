d74c69aae35dad0729b3595fa27f0a49a20955c9
juliano-araujo <julianoaraujo@protonmail.com>
2025-06-30 18:40:03 -0300

    feat: update PetSimpleCard to include an image placeholder and adjust layout styles

--- src/components/pet-simple-card.tsx
+++ src/components/pet-simple-card.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { MoveRight } from "lucide-react";
+import Image from "next/image";
 
 interface Props {
   name: string;
@@ -18,13 +19,18 @@ export function PetSimpleCard({
   return (
     <div
       className={cn(
-        "bg-card text-card-foreground border-border flex flex-col overflow-hidden rounded-lg border",
+        "bg-card text-card-foreground border-border flex max-w-55 flex-col overflow-hidden rounded-lg border",
         className,
       )}
       {...props}
     >
-      <div className="border-border bg-header-background size-32 h-full w-full">
-        Imagem aqui
+      <div className="border-border bg-background relative flex size-32 h-full w-full items-center justify-center">
+        <Image
+          src="/placeholder.webp"
+          alt="Pet image"
+          fill
+          className="object-contain"
+        />
       </div>
       <div className="flex flex-col px-3 py-4">
         <div className="flex items-center justify-between">
